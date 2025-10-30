a9c745932fb4c763579562d116723f1f2fa486ae
Classfied3D <82328013+Classfied3D@users.noreply.github.com>
2025-10-30 20:48:45 +0000

    cli
    Signed-off-by: Classfied3D <82328013+Classfied3D@users.noreply.github.com>

--- llvm2scratch/cli.py
+++ llvm2scratch/cli.py
@@ -1,4 +1,30 @@
-def main():
-  raise Exception("TODO")
+from . import compiler
 
-#debug_branch_info_file: str = "debug_branch_info.txt" # If do_debug_branch_log is enabled, output the info to this file
+from pathlib import Path
+import argparse
+
+def main():
+  parser = argparse.ArgumentParser(description="Process a file with optional optimization.")
+  parser.add_argument("input", type=Path, help="Path to the input ir file")
+  parser.add_argument(
+    "--output",
+    type=Path,
+    default="out.sprite3",
+    help="Path to the output sprite3 file"
+  )
+  parser.add_argument(
+    "--opti",
+    type=lambda x: x.lower() in ["true", "1", "yes", "y"],
+    default=True,
+    help="Enable optimizations (enabled by default)"
+  )
+
+  args = parser.parse_args()
+  cfg = compiler.Config(opti=args.opti)
+
+  with open(args.input, "r") as file:
+    proj, _ = compiler.compile(file.read(), cfg)
+    proj.export(args.output)
+
+if __name__ == "__main__":
+  main()
