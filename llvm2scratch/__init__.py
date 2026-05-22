970913749229df754dbfef5c61194214892b5018
Classfied3D <82328013+Classfied3D@users.noreply.github.com>
2026-05-22 13:41:57 +0100

    fix: re-export correctly

--- llvm2scratch/__init__.py
+++ llvm2scratch/__init__.py
@@ -1,13 +1,10 @@
-from . import optimizer
-from . import compiler
-from . import scratch
+# why is re-exporting so verbose
+from . import optimizer as optimizer
+from . import compiler as compiler
+from . import scratch as scratch
 
-from .optimizer import Optimization, ALL_OPTIMIZATIONS
-from .compiler import Config, DebugInfo, compile
-from .scratch import Project, ScratchConfig, Format
-from .parser import parseAssembly
-
-__all__ = [
-  "optimizer", "compiler", "scratch", "Config", "DebugInfo", "ScratchConfig", "Project", "compile", "parseAssembly",
-  "Optimization", "ALL_OPTIMIZATIONS", "Format",
-]
+from .optimizer import Optimization as Optimization, ALL_OPTIMIZATIONS as ALL_OPTIMIZATIONS
+from .compiler import Config as Config, DebugInfo as DebugInfo, compile as compile
+from .scratch import Project as Project, ScratchConfig as ScratchConfig, Format as Format
+from .parser import parseAssembly as parseAssembly
+from .target import getTarget as getTarget, listTargets as listTargets, DEFAULT_TARGETS as DEFAULT_TARGETS, DEFAULT_OPT_TARGET as DEFAULT_OPT_TARGET
