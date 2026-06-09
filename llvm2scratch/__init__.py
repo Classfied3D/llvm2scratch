a1556316a7b24f018f8e22613e8aa3dd52b3db3e
Classfied3D <82328013+Classfied3D@users.noreply.github.com>
2026-06-09 21:51:21 +0100

    fix: don't export non-existent debug info

--- llvm2scratch/__init__.py
+++ llvm2scratch/__init__.py
@@ -3,12 +3,12 @@ from . import compiler
 from . import scratch
 
 from .optimizer import Optimization, ALL_OPTIMIZATIONS
-from .compiler import Config, DebugInfo, compile
+from .compiler import Config, compile
 from .scratch import Project, ScratchConfig, Format
 from .parser import parseAssembly
 from .target import getTarget, listTargets, DEFAULT_TARGETS, DEFAULT_OPT_TARGET
 
 __all__ = [
-  "optimizer", "compiler", "scratch", "Config", "DebugInfo", "ScratchConfig", "Project", "compile", "parseAssembly",
+  "optimizer", "compiler", "scratch", "Config", "ScratchConfig", "Project", "compile", "parseAssembly",
   "Optimization", "ALL_OPTIMIZATIONS", "Format", "getTarget", "listTargets", "DEFAULT_TARGETS", "DEFAULT_OPT_TARGET",
 ]
